e252dfd460dc230f3c40f08456b143ffb14a0789
ivmu913 <ivmu913@gmail.com>
2023-06-08 19:48:55 +0300

    admin_created

--- svetaine/Rankdarbiai/admin.py
+++ svetaine/Rankdarbiai/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Category, Product, Order, UserProfile, Review, Favorite, Cart, CartItem, Transaction
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'price', 'created_at', 'owner')
+    list_filter = ('category', 'created_at', 'owner')
+    search_fields = ('title', 'description')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'buyer', 'quantity', 'total_price', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('product__title', 'buyer__username')
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'location')
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'created_at')
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product')
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'buyer', 'total_price', 'created_at')
+
+
+
